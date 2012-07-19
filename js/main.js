876347e75ec97cadc10a5f456cc861647f9ba338
Courtney Ardis <ardiscb@fullsail.edu>
2012-07-19 19:00:14 -0400

    Completed project 3: added editItem, deleteItem, and validation functions.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*
 Author: Courtney Ardis 
-Project: Web App Part 2
+Project: Web App Part 3
 Term: 1207
 */
 
@@ -17,7 +17,8 @@ window.addEventListener("DOMContentLoaded", function() {
 	}
 	//Variable defaults
 	var comicGenre = ["--Choose A Genre--", "Superhero", "Horror", "Sci-fi", "Western", "Romance", "Pulp"],
-		styleValue;
+		styleValue,
+		errMsg = e('errors');
 
 	//Select field and options populated
 	function makeGenre(){
@@ -67,8 +68,16 @@ window.addEventListener("DOMContentLoaded", function() {
 	}
 
 	//Function that stores the form fields into Local Storage with a key and value
-	function storeData(){
-		var id    				= Math.floor(Math.random()*100000000001);
+	function storeData(key){
+		//If there is no key, this is a brand new item and we need to generate a key
+		if(!key){
+			var id    			= Math.floor(Math.random()*100000000001);	
+		}else{
+			//Set the id to the existing key that we are editing so that it will save over the data
+			//The key is the same key that's been passed along from the editSubmit event
+			//to the validate function, and then passed here, into the storeData function
+			id = key;
+		}
 		//Gather up all our form field values and store in an object
 		//Object properties contain an array with the form label and input value
 		getSelectedRadio();
@@ -102,6 +111,7 @@ window.addEventListener("DOMContentLoaded", function() {
 		e('items').style.display = "block";
 		for(var i=0, j=localStorage.length; i<j; i++){
 			var makeLi = document.createElement('li');
+			var linksLi = document.createElement('li');
 			makeList.appendChild(makeLi);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
@@ -114,7 +124,88 @@ window.addEventListener("DOMContentLoaded", function() {
 				makeSubList.appendChild(makeSubLi);
 				var optSubText = obj[n][0] + " " + obj[n][1];
 				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
 			}
+			makeItemLinks(localStorage.key(i), linksLi);//Function call for our edit and delete buttons/links
+		}
+	}
+
+	//Make Item Links
+	//Create the edit and delete links for each stored item when displayed
+	function makeItemLinks(key, linksLi){
+		//add edit single item link
+		var editLink = document.createElement('a');
+		editLink.href = "#";
+		editLink.key = key;
+		var editText = "Edit Comic";
+		editLink.addEventListener("click", editItem);
+		editLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+
+		//add line break for links
+		var breakTag = document.createElement('br');
+		linksLi.appendChild(breakTag);
+
+		//add delete single item link
+		var deleteLink = document.createElement('a');
+		deleteLink.setAttribute("id", "deleteA");
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		var deleteText = "Delete Comic";
+		deleteLink.addEventListener("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild(deleteLink);
+	}
+
+	//Function that allows for a single item to be edited
+	function editItem(){
+		//Grab the data from our item in Local Storage
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+
+		//Show the form
+		toggleControls("off");
+
+		//populate the form fields with current Local Storage values
+		e('comicTitle').value 	= item.comicTitle[1];
+		e('seriesTitle').value 	= item.seriesTitle[1];
+		e('issueNum').value 	= item.issueNum[1];
+		e('dateReleased').value = item.dateReleased[1];
+		e('publisher').value 	= item.publisher[1];
+		e('rateIssue').value 	= item.rateIssue[1];
+		e('genre').value 		= item.genre[1];
+		var radios = document.forms[0].illStyle;
+		for(var i=0; i<radios.length; i++){
+			if(radios[i].value == "Full Color" && item.illStyle[1] == "Full Color"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Black & White" && item.illStyle[1] == "Black & White"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "Combination" && item.illStyle[1] == "Combination"){
+				radios[i].setAttribute("checked", "checked");
+			}
+		}
+		e('comments').value 	= item.comments[1];
+
+		//Remove the initial listener from the input 'save comic' button
+		save.removeEventListener("click", storeData);
+		//Change Submit button value to Edit button
+		e('submit').value = "Edit Comic";
+		var editSubmit = e('submit');
+		//Save the key value established in this function as a property of the editSubmit event
+		//so we can use that value when we save the data we edited
+		editSubmit.addEventListener("click", validate);
+		editSubmit.key = this.key;
+	}
+
+	//Function that deletes a single item from Local Storage
+	function deleteItem(){
+		var ask = confirm("Are you sure you want to delete this comic?");
+		if(ask){
+			localStorage.removeItem(this.key);
+			alert("Comic was deleted!");
+			window.location.reload();
+		}else{
+			alert("Comic was NOT deleted!");
 		}
 	}
 
@@ -130,6 +221,63 @@ window.addEventListener("DOMContentLoaded", function() {
 		}
 	}
 
+	//Function that validates input fields
+	function validate(t){
+		//Define the elements we want to check
+		var getComicTitle 	= e('comicTitle');
+		var getIssueNum 	= e('issueNum');
+		var getPublisher 	= e('publisher');
+		var getGenre 		= e('genre');
+
+		//Reset Error Messages
+		errMsg.innerHTML = "";
+		getComicTitle.style.border = "2px inset";
+		getIssueNum.style.border = "2px inset";
+		getPublisher.style.border = "2px inset";
+		getGenre.style.border = "2px inset";
+
+		//Get Error Messages
+		var messageArray = [];
+		//Comic Title Validation
+		if(getComicTitle.value === ""){
+			var comicTitleError = "Please enter a comic title.";
+			getComicTitle.style.border = "1px solid red";
+			messageArray.push(comicTitleError);
+		}
+		//Issue Number Validation
+
+		if(getIssueNum.value === ""){
+			var issueNumError = "Please enter an issue number.";
+			getIssueNum.style.border = "1px solid red";
+			messageArray.push(issueNumError);
+		}
+		//Publisher Validation
+		if(getPublisher.value === ""){
+			var publisherError = "Please enter a publisher.";
+			getPublisher.style.border = "1px solid red";
+			messageArray.push(publisherError);
+		}
+		//Genre Validation
+		if(getGenre.value === "--Choose A Genre--"){
+			var genreError = "Please choose a genre.";
+			getGenre.style.border = "1px solid red";
+			messageArray.push(genreError);
+		}
+
+		//If there were errors, display them on the screen
+		if(messageArray.length >= 1){
+			for(var i=0, j=messageArray.length; i < j; i++){
+				var text = document.createElement('li');
+				text.innerHTML = messageArray[i];
+				errMsg.appendChild(text);
+			}
+			t.preventDefault();
+			return false;
+		}else{
+			//If there are no erros, save our data
+			storeData(this.key);
+		}
+	}
 
 	//Function calls
 	makeGenre();
@@ -140,6 +288,6 @@ window.addEventListener("DOMContentLoaded", function() {
 	var clearLink = e('clearData');
 	clearLink.addEventListener("click", clearLocal);
 	var save = e('submit');
-	save.addEventListener("click", storeData);
+	save.addEventListener("click", validate);
 
 });
